ce7aad16399b69b6a9db714d4ae6925a5735f7e0
samuel.matos <samuel.matos@prof.sc.senac.br>
2026-08-20 21:38:21 -0300

    Aula de banco e swagger

--- src/main/java/com/example/aula20263/repository/UsuarioRepository.java
+++ src/main/java/com/example/aula20263/repository/UsuarioRepository.java
@@ -2,7 +2,9 @@ package com.example.aula20263.repository;
 
 import com.example.aula20263.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 
 }
